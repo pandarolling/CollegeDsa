216b13c2bf9cf12f845e3fe5b7db82e6b14fb0f0
pandarolling <timothykurian@gmail.com>
2026-01-13 16:19:33 +0530

    Simple Interest

--- Lecture_4/2_simpleInterest.cpp
+++ Lecture_4/2_simpleInterest.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main(){
-    
+    float si, rate, time, prin;
+    cout<<"Enter Principle , Rate , and Time for simple interest\n";
+    cin>>prin>>rate>>time;
+    si = (prin*rate*time)/100;
+    cout<<"Output is "<<si<<"\n";
     return 0;
 }
