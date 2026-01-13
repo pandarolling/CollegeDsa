cfc73a0d613746a9289793f301a649ce8f50a31d
pandarolling <timothykurian@gmail.com>
2026-01-13 16:11:01 +0530

    Adding two nums

---
+++ Lecture_4/1_addTwoNumbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b ;
+    cout<<"Input two numbers";
+    cin>>a>>b;
+    int c = a+b;
+    cout<<"\nAns is "<<c<<endl;
+
+    return 0;
+}
