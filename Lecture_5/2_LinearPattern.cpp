8fb94d2aff19c37e381fde048d9d5db25833d8d1
pandarolling <timothykurian@gmail.com>
2026-01-15 15:52:34 +0530

    2nd

---
+++ Lecture_5/2_LinearPattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int i , j;
+int n;
+
+cout<<"\nEnter n";
+cin>>n;
+
+for(i =0; i<n;i++){
+   for(j=0; j<n; j++){
+     cout<<"* ";
+   }
+cout<<endl;
+}
+
+return 0;
+}
