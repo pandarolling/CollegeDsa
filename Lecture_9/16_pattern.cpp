6aa10e565a7156714ce85a64d815eb7ef5712ed7
pandarolling <timothykurian@gmail.com>
2026-01-21 13:52:23 +0530

    Lecture 9

---
+++ Lecture_9/16_pattern.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int m;
+    cin>>m;
+    
+   
+    for(int i=1;i<=m;i++){
+        //outer spaces
+        for(int j=1; j<=i; j++){
+            cout<<"* ";
+        }
+
+        //inner space
+        for(int j=1; j<=(2*(m-i))-1;j++){
+            cout<<"  ";
+        }
+        for(int j=1; j<=i && j!=m; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    } 
+   return 0;
+}
